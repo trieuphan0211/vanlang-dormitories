07bab7d492fa8ccd07ae444dbaf342c630fc89f8
trieuphan <trieuphan0211@gmail.com>
2024-05-30 22:45:45 +0700

    update

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -72,7 +72,7 @@ const Dashboard = ({
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <LineChart invoiceForLineChart={invoice} />
-        <PieChart />
+        {/* <PieChart /> */}
         {/* <ChartTwo /> */}
         {/* <MapOne /> */}
 
